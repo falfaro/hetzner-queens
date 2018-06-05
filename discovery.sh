7c5dbe828c8da011390d5bcf55f26fc0a2ca4b82
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2018-06-05 15:25:30 +0200

    Add missing 'source' to discovery.sh.

--- discovery.sh
+++ discovery.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source ~/stackrc
 openstack overcloud node import instackenv.json
 for node in esjc-ost2-cn01p esjc-ost2-cn02p esjc-ost2-cn03p; do
   ironic node-update $node add properties/root_device='{"size": 48}'
